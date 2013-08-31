4e75a7b26c18005190624e936fccb37533f515e4
Kornelius Kalnbach <murphy@rubychan.de>
2013-08-31 15:45:24 +0200

    nicer name name for Pygmentsrb adapter

--- adapters/pygmentsrb.rb
+++ adapters/pygmentsrb.rb
@@ -4,6 +4,10 @@ module Adapters
   class Pygmentsrb < ShootoutAdapter
     LIBRARY = ::Pygments
     
+    def name
+      'Pygments.rb'
+    end
+    
     def highlight file, source, language, format
       return if format == 'null'
       LIBRARY.highlight(source, lexer: language, formatter: format)
